48b47f1f09804095fc2a4fa9d5482d9e1a38fe0b
Samach21 <69465282+Samach21@users.noreply.github.com>
2021-08-28 18:05:30 +0700

    fixed

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ def floatToRating(a):
 # print('ความแม่นยำเท่ากับ {}%'.format(100*total/all))
 
 print('price.from Distance summary.score atmosphere cleanliness facilities location.y security staff valueformoney')
-print('Ex. : \t\t3300 2.9 9.2 8.9 9.4 9.3 8.9 9.0 9.4 9.4')
+print('Example : \t3300 2.9 9.2 8.9 9.4 9.3 8.9 9.0 9.4 9.4')
 test = list(map(float, input('Enter data: \t').split()))
 print('Result: {}'.format(floatToRating(model_01.predict(np.array([test])).tolist()[0][0])))
