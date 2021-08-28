675cff7c5cf739c23f93df1ccbbc62f8dc9df1b4
Samach21 <69465282+Samach21@users.noreply.github.com>
2021-08-28 18:03:44 +0700

    100%

--- main.py
+++ main.py
@@ -13,22 +13,27 @@ def floatToRating(a):
         a = c 
     else: 
         a = b
-    if a > 4:
-        a = 4
+    if a > 4 or a <= -1:
+        return '''Can't predict'''
     return out[a]
 
-pre_result = []
-for _, value in testY.items():
-    pre_result.append(value)
+# pre_result = []
+# for _, value in testY.items():
+#     pre_result.append(value)
 
-result = []
-for arr in ls:
-    result.append(floatToRating(model_01.predict(np.array([arr])).tolist()[0][0]))
+# result = []
+# for arr in ls:
+#     result.append(floatToRating(model_01.predict(np.array([arr])).tolist()[0][0]))
 
-all = len(pre_result)
-total = 0
-for a, b in zip(pre_result, result):
-    if a == b:
-        total += 1  
+# all = len(pre_result)
+# total = 0
+# for a, b in zip(pre_result, result):
+#     if a == b:
+#         total += 1  
 
-print('ความแม่นยำเท่ากับ {}%'.format(100*total/all))
+# print('ความแม่นยำเท่ากับ {}%'.format(100*total/all))
+
+print('price.from Distance summary.score atmosphere cleanliness facilities location.y security staff valueformoney')
+print('Ex. : \t\t3300 2.9 9.2 8.9 9.4 9.3 8.9 9.0 9.4 9.4')
+test = list(map(float, input('Enter data: \t').split()))
+print('Result: {}'.format(floatToRating(model_01.predict(np.array([test])).tolist()[0][0])))
